46b0cb1b68d3ac75e1cc646a60b5ba974901ab12
nbouteme <nbouteme@users.noreply.github.com>
2018-03-11 15:34:35 +0100

    Define some host side structures

---
+++ x64/main.s
@@ -0,0 +1,26 @@
+%include "event_loop.inc"
+%include "host.inc"
+
+segment .bss
+el: resb event_loop.size
+hts: resb host_tcp_server.size
+hue: resb host_udp_emitter.size
+
+segment .text
+
+global _start
+_start:
+	mov rdi, el
+	call init_event_loop
+	mov rdi, hts
+	call init_host_tcp_server
+	mov rdi, hue
+	call init_host_udp_emitter
+	mov rdi, el
+	mov rsi, hts
+	call add_source
+	mov rdi, el
+	mov rsi, hue
+	call add_source
+	mov rdi, el
+	call run_event_loop
