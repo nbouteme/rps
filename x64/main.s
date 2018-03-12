750ad6c0b7109627403a55e9f80a0eb09dd482ff
nbouteme <nbouteme@users.noreply.github.com>
2018-03-12 11:01:47 +0100

    Initialize host structures instead of calling functions

--- x64/main.s
+++ x64/main.s
@@ -2,21 +2,44 @@
 %include "host.inc"
 %include "syscalls.inc"
 
+segment .text
+extern accept_tcp
+extern udp_receive
+
+segment .data
+extern host_tcp_pdata
+extern host_udp_pdata
+
+tcp_server_str: db 'Host TCP Server', 0
+udp_emitter_str: db 'Host UDP Emitter', 0
+
+hts:
+istruc host_tcp_server
+at host_tcp_server.base, istruc source
+							 at source.name,    dq tcp_server_str
+							 at source.pfd,     dq 0,
+							 at source.receive, dq accept_tcp
+						 iend
+at host_tcp_server.priv, dq host_tcp_pdata
+iend
+
+hue:
+istruc host_udp_emitter
+at host_udp_emitter.base, istruc source
+							 at source.name,    dq udp_emitter_str
+							 at source.pfd,     dq 0,
+							 at source.receive, dq udp_receive
+						  iend
+at host_tcp_server.priv, dq host_udp_pdata
+iend
+
 segment .bss
 el: resb event_loop.size
-hts: resb host_tcp_server.size
-hue: resb host_udp_emitter.size
 
 segment .text
 
 global _start
 _start:
-	mov rdi, el
-	call init_event_loop
-	mov rdi, hts
-	call init_host_tcp_server
-	mov rdi, hue
-	call init_host_udp_emitter
 	mov rdi, el
 	mov rsi, hts
 	call add_source
